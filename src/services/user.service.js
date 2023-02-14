3d8be0ed920aa451233f140566893f947ecd9bbb
hibo2261 <hborse1@gmail.com>
2023-02-15 00:30:24 +0530

    [Himanshu]Added : RabbitMQ Is Sucessfully Implemented.

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,74 +1,72 @@
-import User from '../models/user.model';
-const bcrypt = require('bcrypt')
 import jwt from 'jsonwebtoken';
-import {sendMail} from '../utils/user.util';
+import User from '../models/user.model';
+import { sendMail } from '../utils/user.util';
+// import { producer } from '../utils/rabbitmq';
+import { sender } from '../config/rabbit';
+// import logger from '../config/logger';
 
+const bcrypt = require('bcrypt');
 
-
-//get all users
+// get all users
 export const getAllUsers = async () => {
   const data = await User.find();
   return data;
 };
 
-
-//create new user
+// create new user
 export const userRegistration = async (body) => {
   const existingUser = await User.findOne({ email: body.email });
   if (!existingUser) {
     const salt = await bcrypt.genSalt(10);
     body.password = await bcrypt.hash(body.password, salt);
     const data = await User.create(body);
+    // producer(data);
+    sender(data);
     return data;
   }
-  else {
-    throw new Error("User already exist")
-  }
+
+  throw new Error('User already exist');
 };
 
-//User Login Validation
+// User Login Validation
 export const login = async (body) => {
   try {
     const userdata = await User.findOne({ email: body.email });
     if (!userdata) {
-      throw new Error("Please Enter Valid Email....")
+      throw new Error('Please Enter Valid Email....');
     }
-    const validPassword = await bcrypt.compare(body.password,userdata.password);
-    if(!validPassword){
-      throw new Error("Please Enter Valid Password....")
+    const validPassword = await bcrypt.compare(body.password, userdata.password);
+    if (!validPassword) {
+      throw new Error('Please Enter Valid Password....');
     }
- 
-    let token = jwt.sign({email : userdata.email, id: User._id  },process.env.JWT_SECRET_KEY)
 
+    const token = jwt.sign({ email: userdata.email, id: User._id }, process.env.JWT_SECRET_KEY);
+    // console.log("---------------------------------------------------------------------------------------",token)
+     sender(userdata);
     return token;
+  } catch (error) {
+    throw new Error(error);
   }
-  catch (error) {
-    throw new Error(error)
-  }
-
 };
 
-
-//--------> Forget password
-export const  forgetPassword = async (body) => {
-
-  
-  try{
+// --------> Forget password
+export const forgetPassword = async (body) => {
+  try {
     const data = await User.findOne({ email: body.email });
-    console.log("---------------",data)
-      const token = await jwt.sign({ email : data.email , _id : User._id }, process.env.PASSWORDKEY);
- console.log("-------------------------------------------",token);
-     const mailsend = await sendMail(data.email, token);
+    console.log('---------------', data);
+    const token = await jwt.sign({ email: data.email, _id: User._id }, process.env.PASSWORDKEY);
+    console.log('-------------------------------------------', token);
+    const mailsend = await sendMail(data.email, token);
     return mailsend;
-  } catch{
-    throw new Error("Email not found");
+  } catch {
+    throw new Error('Email not found');
   }
-}
+};
 
 export const resetPassword = async (token, body) => {
   const saltRounds = 10;
-  const passwordHash = await bcrypt.hash(body.password,saltRounds);
-  body.password = passwordHash; 
+  const passwordHash = await bcrypt.hash(body.password, saltRounds);
+  body.password = passwordHash;
   const data = User.findOneAndUpdate(
     {
       email: body.email
@@ -78,6 +76,7 @@ export const resetPassword = async (token, body) => {
     },
     {
       new: true
-    })
+    }
+  );
   return data;
 };
